adca6cdd0c24cc67015b662b41a2fd4a873ae927
Shawn Xue <shawn.xue@mercedes-benz.com>
2025-02-25 17:27:48 -0800

    update

--- exercises/exercise_07.py
+++ exercises/exercise_07.py
@@ -1,25 +1,26 @@
-# Given a Binary Tree, check if all leaves are at same level or not. 
+# Given a Binary Tree, check if all leaves are at same level or not.
 class Node:
   def __init__(self, data):
     self.data = data
     self.left = None
     self.right = None
-  
+
 def areAllLeavesSamelevel(root: Node):
+  current_leaf_level = 0
+  first_leaf_level = 0
+
   def isSameLevel(root, current_leaf_level):
     if root is None:
       return True
     # found a leaf
     if root.left is None and root.right is None:
       # this is the first time to find a leaf
-      if first_leaf_level == 0: 
+      if first_leaf_level == 0:
         first_leaf_level = current_leaf_level
         return True
       # this is not the first time to find leaf
       return current_leaf_level == first_leaf_level
     # not leaf yet
     return (isSameLevel(root.left, current_leaf_level + 1) and isSameLevel(root.right, current_leaf_level + 1))
-  
-  current_leaf_level = 0
-  first_leaf_level = 0
+
   return isSameLevel(root, current_leaf_level)
--- exercises/exercise_08.py
+++ exercises/exercise_08.py
@@ -1,10 +1,11 @@
 def testVariableInRec():
   def recMethod(lev):
     print(f"level_in_rec_before={lev}")
+    print(f"check={check}")
     lev += 1
     print(f"level_in_rec_after={lev}")
     print(f"check={check}")
-    print(f"check={level}")
+
   level = 10
   check = 5
   print(f"level_outside_rec_before={level}")
